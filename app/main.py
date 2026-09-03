e590c19687146d95296002e440bd2e5c203c7902
DylanS0ares <bdylansoares@gmail.com>
2026-09-03 11:00:36 -0300

    Remove arquivos/pastas indesejados; mantém apenas backend, frontend e README

--- app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from .routers import veiculos,clientes,locacoes,usuarios
-
-app = FastAPI()
-
-app.include_router(veiculos.router)
-app.include_router(clientes.router)
-app.include_router(locacoes.router)
-app.include_router(usuarios.router)
